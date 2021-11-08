ff1e3a910790825fd90e2ccfaa063f27059f2eb6
Adrian Walker <awalker3365@gmail.com>
2021-11-08 16:38:55 -0500

    added inputs & images

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import './style.css';
 
 function App() {
   return (
+    // Main App
     <div className="app">
+      {/* inputs */}
+      <div className="inputs">
+        <ul>
+          <li><input placeholder="Title"></input></li>
+          <li><input placeholder="Img URL"></input></li>
+          <li><input placeholder="Description"></input></li>
+        </ul>
+        <button className="submit">Submit</button>
+      </div>
+
+      {/* Images */}
+      <div>
+        <ul>
+          <li><img src="" alt="" /></li>
+          <li><img src="" alt="" /></li>
+          <li><img src="" alt="" /></li>
+        </ul>
+      </div>
 
     </div>
   );
