65a9581e9c05927b9569c43c561bc0c509733cdf
Adrian Walker <awalker3365@gmail.com>
2021-11-19 21:51:11 -0500

    next

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 function App() {
+  const [thing, setthing] = useState(initialState)
   return (
     // Main App
     <div className="app">
       {/* inputs */}
       <div className="inputs">
         <ul>
-          <li><input placeholder="Title"></input></li>
-          <li><input placeholder="Img URL"></input></li>
-          <li><input placeholder="Description"></input></li>
+          <li className="item"><input placeholder="Title"></input></li>
+          <li className="item"><input placeholder="Img URL"></input></li>
+          <li className="item"><input placeholder="Description"></input></li>
         </ul>
         <div className='buttonDiv'>
           <button className="submit">Submit</button>
-        </div>
 
+        </div>
       </div>
 
       {/* Images */}
